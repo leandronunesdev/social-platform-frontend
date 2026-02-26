e2d492e654c7852b5a15ce04c1b401b3f04b564f
Leandro <nunes.pessoal@gmail.com>
2026-02-26 18:16:24 -0300

    adjustments from cypress e2e tests insights

--- cypress/e2e/profile.cy.ts
+++ cypress/e2e/profile.cy.ts
@@ -26,7 +26,7 @@ describe("Profile registration flow", () => {
 
     cy.get('textarea[id="bio"]').type("This is a bio");
     cy.get('input[id="country"]').type("Brazil");
-    cy.get('input[id="state"]').type("Parana");
+    cy.get('input[id="state"]').type("Paraná");
     cy.get('input[id="city"]').type("Curitiba");
     cy.get('input[id="avatar"]').type("https://i.pravatar.cc/300");
 
